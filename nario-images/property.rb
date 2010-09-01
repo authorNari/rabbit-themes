902473ba44e026857f34714d88d2a89fc6335de1
Narihiro Nakamura <authornari@gmail.com>
2010-09-01 11:28:08 +0900

    first commit

---
+++ nario-images/property.rb
@@ -0,0 +1,5 @@
+@category = N_("Image")
+@title = N_("Nario Image")
+@abstract = N_("Images related to ((<zen-nari-style>)) theme")
+@description = N_("Provides images related to ((<zen-nari-style>)) theme.")
+~
